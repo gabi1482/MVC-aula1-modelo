da4d4b00d4d58aa772a909c4bc6df721fa72602a
ADM <ADM@DESKTOP-BVUKACK>
2024-03-14 11:53:16 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FormularioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_aula1.Models;
+using System.Diagnostics;
+
+namespace MVC_aula1.Controllers
+{
+    public class FormularioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
